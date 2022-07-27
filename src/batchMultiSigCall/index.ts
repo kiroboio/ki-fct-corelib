38a5b99758d941d057399e5cc1b0cf6b56f9a48a
Brenzee <karelisbrendons@gmail.com>
2022-07-27 11:49:59 +0300

    Fix for the FD/Validator functionality

--- src/batchMultiSigCall/index.ts
+++ src/batchMultiSigCall/index.ts
@@ -315,8 +315,6 @@ export class BatchMultiSigCall {
       },
     };
 
-    console.log(JSON.stringify(typedData, null, 2));
-
     const encodedMessage = ethers.utils.hexlify(
       TypedDataUtils.encodeData(typedData, typedData.primaryType, typedData.message)
     );
