4f2fae204511738443e0d0ef7caa8def50390ef2
Brenzee <karelisbrendons@gmail.com>
2022-09-19 13:46:04 +0300

    Update batchMultiSigCall to support new version of plugins

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 export { BatchMultiSigCall } from "./batchMultiSigCall";
 export { getPlugin, getPlugins } from "@kirobo/ki-eth-fct-provider-ts";
+
 import utils from "./utils";
 import constants from "./constants";
 
 export { utils, constants };
+
+export type {
+  IPlugins,
+  Plugin,
+  PluginInputParams,
+  PluginInstance,
+  PluginMethod,
+  PluginOutputParams,
+  PluginProtocol,
+  PluginType,
+} from "@kirobo/ki-eth-fct-provider-ts";
