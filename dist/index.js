aee4726022c2a48aa09a5b526fada5c1dadd10e1
Brenzee <karelisbrendons@gmail.com>
2022-09-21 12:52:38 +0300

    Set export everything from plugins

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.constants = exports.utils = exports.FctParam = exports.ERRORS = exports.getPlugins = exports.getPlugin = exports.BatchMultiSigCall = void 0;
+exports.constants = exports.utils = exports.BatchMultiSigCall = void 0;
 const utils_1 = __importDefault(require("./utils"));
 exports.utils = utils_1.default;
 const constants_1 = __importDefault(require("./constants"));
 exports.constants = constants_1.default;
 var batchMultiSigCall_1 = require("./batchMultiSigCall");
 Object.defineProperty(exports, "BatchMultiSigCall", { enumerable: true, get: function () { return batchMultiSigCall_1.BatchMultiSigCall; } });
-var ki_eth_fct_provider_ts_1 = require("@kirobo/ki-eth-fct-provider-ts");
-Object.defineProperty(exports, "getPlugin", { enumerable: true, get: function () { return ki_eth_fct_provider_ts_1.getPlugin; } });
-Object.defineProperty(exports, "getPlugins", { enumerable: true, get: function () { return ki_eth_fct_provider_ts_1.getPlugins; } });
-Object.defineProperty(exports, "ERRORS", { enumerable: true, get: function () { return ki_eth_fct_provider_ts_1.ERRORS; } });
-Object.defineProperty(exports, "FctParam", { enumerable: true, get: function () { return ki_eth_fct_provider_ts_1.FctParam; } });
+__exportStar(require("@kirobo/ki-eth-fct-provider-ts"), exports);
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,7 @@ import utils from "./utils";
 import constants from "./constants";
 
 export { BatchMultiSigCall } from "./batchMultiSigCall";
-export { getPlugin, getPlugins, ERRORS, FctParam } from "@kirobo/ki-eth-fct-provider-ts";
 
 export { utils, constants };
 
-export type {
-  IPlugins,
-  Plugin,
-  PluginInputParams,
-  PluginInstance,
-  PluginMethod,
-  PluginOutputParams,
-  PluginProtocol,
-  PluginType,
-} from "@kirobo/ki-eth-fct-provider-ts";
+export * from "@kirobo/ki-eth-fct-provider-ts";
