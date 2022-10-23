7eedfeae4848e714081a2b1fec7248f801a484ce
Brenzee <karelisbrendons@gmail.com>
2022-10-23 13:51:55 +0300

    FCT Script

--- scripts/fct.script.ts
+++ scripts/fct.script.ts
@@ -21,35 +21,30 @@ function addHours(numOfHours: number, date = new Date()) {
   return Number(date.getTime() / 1000).toFixed();
 }
 
-// function addMinutes(numOfMinutes: number, date = new Date()) {
-//   date.setTime(date.getTime() + numOfMinutes * 60 * 1000);
-
-//   return Number(date.getTime() / 1000).toFixed();
-// }
-
-const FCT_Controller_Rinkeby = "0x5a59026F30Df81F482816350E50b27285D84E9c8";
-const FCT_Controller_Goerli = "0xE215Fe5f574593A034c7E6e9BE280A254D02F4dd";
-const Rinkeby_USDT = "0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02";
-const Mainnet_USDC = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
-const Goerli_USDC = "0x2f3A40A3db8a7e3D09B0adfEfbCe4f6F81927557";
-
-const data = {
-  KIRO_GOERLI: "0xba232b47a7dDFCCc221916cf08Da03a4973D3A1D",
-  PUREVALIDATOR_GOERLI: "0x1716898d72BE098F4828B435a3918cBFda562Efc",
-  PURESAFEMATH_GOERLI: "0x850c7E3eBf05d0A617DAe4beE14A4A5C03CAb9da",
-};
-
+const chainId = 5;
 const wallet = "0x62e3A53A947D34C4DdCD67B49fAdc30b643e2586";
 
-// GOERLI RPC URL - https://eth-goerli.public.blastapi.io
+const data = {
+  1: {
+    USDC: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  },
+  5: {
+    FCT_Controller: "0xE215Fe5f574593A034c7E6e9BE280A254D02F4dd",
+    USDC: "0x2f3A40A3db8a7e3D09B0adfEfbCe4f6F81927557",
+    KIRO: "0xba232b47a7dDFCCc221916cf08Da03a4973D3A1D",
+    PureValidator: "0x1716898d72BE098F4828B435a3918cBFda562Efc",
+    PureSafeMath: "0x850c7E3eBf05d0A617DAe4beE14A4A5C03CAb9da",
+    rpcUrl: "https://eth-goerli.public.blastapi.io",
+  },
+};
 
 async function main() {
   const batchMultiSigCall = new BatchMultiSigCall({
-    provider: new ethers.providers.JsonRpcProvider("https://eth-goerli.public.blastapi.io"),
-    contractAddress: FCT_Controller_Goerli,
+    provider: new ethers.providers.JsonRpcProvider(data[chainId].rpcUrl),
+    contractAddress: data[chainId].FCT_Controller,
     options: {
-      // validFrom: addHours(0.66), // UNIX timestamp
-      expiresAt: getDate(10), // UNIX timestamp
+      // validFrom: addHours(0.66),
+      expiresAt: getDate(10),
     },
   });
 
@@ -59,7 +54,7 @@ async function main() {
   const balanceOf = new ERC20.getters.BalanceOf({
     chainId: 1,
     initParams: {
-      to: data.KIRO_GOERLI,
+      to: data[chainId].KIRO,
       methodParams: {
         owner: vault,
       },
@@ -69,7 +64,7 @@ async function main() {
   const greaterThan = new PureValidator.validate.GreaterThan({
     chainId: 1,
     initParams: {
-      to: data.PUREVALIDATOR_GOERLI,
+      to: data[chainId].PureValidator,
       methodParams: {
         value1: balanceOf.output.params.balance.getOutputVariable("1"),
         value2: "10",
@@ -80,7 +75,7 @@ async function main() {
   const transfer = new ERC20.actions.Transfer({
     chainId: 1,
     initParams: {
-      to: data.KIRO_GOERLI,
+      to: data[chainId].KIRO,
       methodParams: {
         recipient: wallet,
         amount: "10",
