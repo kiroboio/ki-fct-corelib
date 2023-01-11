e82792484287827c2316c1e59a9ce56a758deefb
Brenzee <karelisbrendons@gmail.com>
2023-01-11 08:43:13 +0200

    Update addresses

--- dist/batchMultiSigCall/data.js
+++ dist/batchMultiSigCall/data.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const FCTControllerAddresses = {
-    1: "0xa36457BCa36Ac0eED36f1b25bd9144B029600982",
-    5: "0xa36457BCa36Ac0eED36f1b25bd9144B029600982",
+    1: "0xc86A8f32feDB6D05b9153049aA596cF5C1621d45",
+    5: "0xc86A8f32feDB6D05b9153049aA596cF5C1621d45",
 };
 exports.default = FCTControllerAddresses;
--- src/batchMultiSigCall/data.ts
+++ src/batchMultiSigCall/data.ts
@@ -1,6 +1,6 @@
 const FCTControllerAddresses = {
-  1: "0xa36457BCa36Ac0eED36f1b25bd9144B029600982",
-  5: "0xa36457BCa36Ac0eED36f1b25bd9144B029600982",
+  1: "0xc86A8f32feDB6D05b9153049aA596cF5C1621d45",
+  5: "0xc86A8f32feDB6D05b9153049aA596cF5C1621d45",
 };
 
 export default FCTControllerAddresses;
