13baf8ecfc38894aa5eadb35dac756a4691fef7c
Brenzee <karelisbrendons@gmail.com>
2023-02-13 13:27:37 +0200

    Update maxRepeats validator

--- src/batchMultiSigCall/helpers/checkers.ts
+++ src/batchMultiSigCall/helpers/checkers.ts
@@ -16,8 +16,12 @@ const validateInteger = (value: string, keys: string[]) => {
   if (value.startsWith("-")) {
     throw new Error(`Options: ${keys.join(".")} cannot be negative`);
   }
-  if (currentKey === "maxRepeats" && Number(value) < 2) {
-    throw new Error(`Options: ${keys.join(".")} should be at least 2`);
+  if (currentKey === "maxRepeats" && Number(value) < 1) {
+    throw new Error(
+      `Options: ${keys.join(
+        "."
+      )} should be at least 1. If value is 1, recurrency will not be enabled in order to save gas`
+    );
   }
 };
 
