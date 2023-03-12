05a6ed8a5e87bb080cccaec48db032f3a658b466
Brenzee <karelisbrendons@gmail.com>
2023-03-12 16:22:25 +0200

    Fix: importFCT fix for tuple params

--- src/batchMultiSigCall/classes/FCTCalls/helpers.ts
+++ src/batchMultiSigCall/classes/FCTCalls/helpers.ts
@@ -1,6 +1,7 @@
 import { BigNumber, ethers, utils } from "ethers";
+import { ParamType } from "ethers/lib/utils";
 
-import { Param } from "../../../types";
+import { FCTCallParam, Param, TypedDataTypes, Variable } from "../../../types";
 
 export const isInteger = (value: string, key: string) => {
   if (value.length === 0) {
@@ -99,4 +100,97 @@ export const getParamsFromInputs = (inputs: ethers.utils.ParamType[], values: et
       value,
     };
   });
+};
+
+export const getParamsFromTypedData = ({
+  methodInterfaceParams,
+  parameters,
+  types,
+  primaryType,
+}: {
+  methodInterfaceParams: ethers.utils.ParamType[];
+  parameters: Record<string, FCTCallParam>;
+  types: TypedDataTypes;
+  primaryType: string;
+}) => {
+  const generateRealInputParams = (types: TypedDataTypes, primaryType: string) => {
+    const type = types[primaryType];
+    // If the type[0] name is call and type is Call, then slice the first element
+    if (type[0].name === "call" && type[0].type === "Call") {
+      type.shift();
+    }
+    const params: ParamType[] = [];
+    for (const { name, type: paramType } of type) {
+      // Remove [] from the end of the type
+      const typeWithoutArray = paramType.replace(/\[\]$/, "");
+
+      if (types[typeWithoutArray]) {
+        const components = generateRealInputParams(types, typeWithoutArray);
+        params.push(ParamType.from({ name, type: typeWithoutArray, components }));
+      } else {
+        params.push(ParamType.from({ name, type: paramType }));
+      }
+    }
+    return params;
+  };
+
+  const getParams = (
+    realInputParams: ParamType[],
+    eip712InputTypes: ParamType[],
+    parameters: Record<string, FCTCallParam>
+  ) => {
+    return eip712InputTypes.map((input, i): Param => {
+      const realInput = realInputParams[i];
+      if (input.type === "tuple") {
+        return {
+          name: realInput.name,
+          type: input.type,
+          customType: true,
+          value: getParams(
+            realInput.components,
+            input.components,
+            parameters[realInput.name] as Record<string, FCTCallParam>
+          ),
+        };
+      }
+      if (input.type === "tuple[]") {
+        return {
+          name: realInput.name,
+          type: input.type,
+          customType: true,
+          value: (parameters[realInput.name] as Record<string, FCTCallParam>[]).map((tuple) =>
+            getParams(realInput.components, input.components, tuple)
+          ),
+        };
+      }
+      let value = parameters[realInput.name] as boolean | string | Variable | number;
+      // Check if value isn't a variable
+      const variables = ["0xfb0", "0xfa0", "0xfc00000", "0xfd00000", "0xfdb000"];
+
+      if (BigNumber.isBigNumber(value)) {
+        const hexString = value.toHexString().toLowerCase();
+        if (variables.some((v) => hexString.startsWith(v))) {
+          value = hexString;
+        }
+        value = value.toString();
+      }
+
+      if (typeof value === "number") {
+        value = value.toString();
+      }
+
+      return {
+        name: realInput.name,
+        type: realInput.type,
+        customType: false,
+        // If realInputType.type is a string and eip712InputType.type is bytes32, value is hashed
+        hashed: input.type === "bytes32" && realInput.type === "string",
+        value,
+      };
+    });
+  };
+
+  const realInputParams = generateRealInputParams(types, primaryType);
+
+  return getParams(realInputParams, methodInterfaceParams, parameters);
 };
