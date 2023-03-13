d5fbebdbceaeed9e5cd12dfd92ab2c00d824f5d2
GitHub Action <github-action@users.noreply.github.com>
2023-03-13 15:31:59 +0000

    Build

--- dist/utils/index.js
+++ dist/utils/index.js
@@ -17,3 +17,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./FCT"), exports);
 __exportStar(require("./fetch"), exports);
 __exportStar(require("./gas"), exports);
+__exportStar(require("./getKIROPrice"), exports);
