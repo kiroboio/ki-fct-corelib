8eabadd672acc29782dfc8064f97929c7f8d12b1
Brenzee <karelisbrendons@gmail.com>
2023-03-16 10:58:18 +0200

    Fix: getGasPrice fix for mainnet

--- scripts/estimate.script.ts
+++ scripts/estimate.script.ts
@@ -15,6 +15,7 @@ const USDT = "0xc2132d05d31c914a87c6611c10748aeb04b58e8f";
 async function main() {
   const gasPrices = await utils.getGasPrices({
     rpcUrl: scriptData[chainId].rpcUrl,
+    chainId: Number(chainId),
   });
 
   console.log(gasPrices);
--- src/utils/gas.ts
+++ src/utils/gas.ts
@@ -79,10 +79,12 @@ export const transactionValidator = async (
 
 export const getGasPrices = async ({
   rpcUrl,
+  chainId: chainIdParam,
   historicalBlocks = 10,
   tries = 40,
 }: {
   rpcUrl: string;
+  chainId?: number;
   historicalBlocks?: number;
   tries?: number;
 }): Promise<Record<"slow" | "average" | "fast" | "fastest", EIP1559GasPrice>> => {
@@ -91,6 +93,7 @@ export const getGasPrices = async ({
     return Math.round(sum / arr.length);
   }
   const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+  const { chainId } = chainIdParam ? { chainId: chainIdParam } : await provider.getNetwork();
 
   let keepTrying = true;
   let returnValue: Record<"slow" | "average" | "fast" | "fastest", EIP1559GasPrice>;
@@ -104,20 +107,24 @@ export const getGasPrices = async ({
       const baseFee = latestBlock.baseFeePerGas.toString();
       const blockNumber = latestBlock.number;
 
-      console.log(blockNumber);
+      const generateBody = () => {
+        return JSON.stringify({
+          jsonrpc: "2.0",
+          method: "eth_feeHistory",
+          params: [historicalBlocks, `0x${blockNumber.toString(16)}`, [2, 5, 10, 25]],
+          id: 1,
+        });
+      };
 
       const res = await fetch(rpcUrl, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          jsonrpc: "2.0",
-          method: "eth_feeHistory",
-          params: [`0x${historicalBlocks.toString(16)}`, `0x${blockNumber.toString(16)}`, [2, 5, 10, 25]],
-        }),
+        body: generateBody(),
       });
-      const { result } = await res.json();
+      const data = await res.json();
+      const result = data.result;
 
       if (!result) {
         throw new Error("No result");
