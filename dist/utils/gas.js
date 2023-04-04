814addf86721a87bd5d1ce3d35e7865df2e4120d
GitHub Action <github-action@users.noreply.github.com>
2023-04-04 15:57:55 +0000

    Build

--- dist/utils/gas.js
+++ dist/utils/gas.js
@@ -80,7 +80,7 @@ const getGasPrices = async ({ rpcUrl, chainId: chainIdParam, historicalBlocks = 
                 return JSON.stringify({
                     jsonrpc: "2.0",
                     method: "eth_feeHistory",
-                    params: [historicalBlocks, `0x${blockNumber.toString(16)}`, [2, 5, 10, 25]],
+                    params: [historicalBlocks, `0x${blockNumber.toString(16)}`, [2, 5, 15, 25]],
                     id: 1,
                 });
             };
