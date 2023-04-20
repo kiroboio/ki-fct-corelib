c4d896ce434d3b3234df9898ac66b58c22f7c611
GitHub Action <github-action@users.noreply.github.com>
2023-04-20 12:32:59 +0000

    Build

--- dist/batchMultiSigCall/classes/FCTUtils/getPaymentPerPayer.d.ts
+++ dist/batchMultiSigCall/classes/FCTUtils/getPaymentPerPayer.d.ts
@@ -1,1 +1,15 @@
+import { MSCall } from "../../types";
 export declare function getTotalApprovalCalls(pathIndexes: string[], calls: any[]): void;
+export declare function getPayersForRoute({ calls, pathIndexes, calldata, signatureCount, }: {
+    calls: MSCall[];
+    pathIndexes: string[];
+    calldata: string;
+    signatureCount: number;
+}): {
+    payer: string;
+    gas: bigint;
+}[];
+export declare function getEffectiveGasPrice({ maxGasPrice, gasPrice, }: {
+    maxGasPrice: string | bigint;
+    gasPrice: string | bigint;
+}): string;
