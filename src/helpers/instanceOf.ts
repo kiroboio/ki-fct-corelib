9f6dbff10681b8e0ca83e388244b5094dc90e96c
Brenzee <karelisbrendons@gmail.com>
2023-06-20 15:00:49 +0300

    Remove comments

--- src/helpers/instanceOf.ts
+++ src/helpers/instanceOf.ts
@@ -1,17 +1,5 @@
 import { IWithPlugin, Param, Variable } from "../types";
 
-// export const instanceOfVariable = (object: any): object is Variable => {
-//   return typeof object === "object" && "type" in object && "id" in object;
-// };
-//
-// export function instanceOfParams(objectOrArray: any): objectOrArray is Param | Param[] {
-//   if (Array.isArray(objectOrArray)) {
-//     return instanceOfParams(objectOrArray[0]);
-//   }
-//
-//   return typeof objectOrArray === "object" && "type" in objectOrArray && "name" in objectOrArray;
-// }
-
 export class InstanceOf {
   static Variable = (object: any): object is Variable => {
     return typeof object === "object" && "type" in object && "id" in object;
