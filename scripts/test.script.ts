efc241e9829dd9aac86c20a1a2b4d787b1115f04
Brenzee <karelisbrendons@gmail.com>
2023-07-30 13:00:53 +0300

    Precentiles for evert chain seperately

--- scripts/test.script.ts
+++ scripts/test.script.ts
@@ -1,40 +1,18 @@
 import { ethers } from "ethers";
 
-import { BatchMultiSigCall } from "../src";
+import { getGasPrices } from "../src/utils";
+import scriptData from "./scriptData";
 
 const createRandomAddress = () => ethers.Wallet.createRandom().address;
+const chainId = 5;
 
 async function main() {
-  const FCT = new BatchMultiSigCall();
-  await FCT.create({
-    nodeId: "node1",
-    to: createRandomAddress(),
-    toENS: "@token.kiro.eth",
-    method: "transfer",
-    params: [
-      { name: "to", type: "address", value: createRandomAddress() },
-      { name: "token_amount", type: "uint256", value: "20" },
-    ],
-    from: createRandomAddress(),
-    value: "0",
+  const gasPrices = await getGasPrices({
+    rpcUrl: scriptData[chainId].rpcUrl,
+    chainId: chainId,
   });
 
-  await FCT.create({
-    nodeId: "node2",
-    to: createRandomAddress(),
-    method: "erc20Airdrop",
-    params: [
-      { name: "token", type: "address", value: createRandomAddress() },
-      { name: "from", type: "address", value: createRandomAddress() },
-      { name: "amount", type: "uint256", value: createRandomAddress() },
-      { name: "recipients", type: "address[1]", value: [createRandomAddress()] },
-    ],
-    from: createRandomAddress(),
-  });
-
-  const FCTData = FCT.exportFCT();
-
-  console.log(JSON.stringify(FCTData, null, 2));
+  console.log(gasPrices);
 }
 
 main()
