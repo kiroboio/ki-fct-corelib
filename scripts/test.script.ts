18d16604be4d6b1cf4cf59c927662e53ad713e9a
Brenzee <karelisbrendons@gmail.com>
2023-11-27 09:38:15 +0200

    fix(generaltype): fix param.value type

--- scripts/test.script.ts
+++ scripts/test.script.ts
@@ -12,32 +12,7 @@ async function main() {
     chainId: "1",
   });
 
-  FCT.addComputed({
-    id: "32",
-    type: "computed",
-    value1: "1",
-    operator1: "+",
-    value2: "12",
-    operator2: "+",
-    value3: "0",
-    operator3: "/",
-    value4: "1000",
-    overflowProtection: true,
-  });
-
-  await FCT.add({
-    nodeId: "966b50af-b432-43af-aae3-f9f1014c7900",
-    to: "0x4c508dc4a3aacbecbf13c1d543b4936274033110",
-    from: "0x4c508dc4a3aacbecbf13c1d543b4936274033110",
-    method: "activate",
-    params: [
-      {
-        value: { type: "computed", id: "32" },
-        type: "uint256",
-        name: "amount",
-      },
-    ],
-  });
+  const
 
   // FCT.addComputed({
   //   id: "0d286110-3ea4-4f65-a69e-a1afb3b71585",
