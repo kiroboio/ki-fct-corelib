ba37679f7439330acd06129f1172437a5c08f7a5
Brenzee <karelisbrendons@gmail.com>
2024-03-17 10:50:58 +0200

    use rollup to generate dist

---
+++ rollup.config.mjs
@@ -0,0 +1,39 @@
+// Need @rollup/plugin-json
+import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+function getConfig(opts) {
+  if (opts == null) {
+    opts = {};
+  }
+
+  const file = `./dist/core${opts.suffix || ""}.js`;
+  const exportConditions = ["import", "default"];
+  const mainFields = ["module", "main"];
+  if (opts.browser) {
+    mainFields.unshift("browser");
+  }
+
+  return {
+    input: "./lib.esm/index.js",
+    output: {
+      inlineDynamicImports: true,
+      file,
+      name: opts.name || undefined,
+      format: opts.format || "esm",
+      sourcemap: true,
+    },
+    treeshake: true,
+    plugins: [
+      nodeResolve({
+        exportConditions,
+        mainFields,
+        modulesOnly: true,
+        preferBuiltins: false,
+      }),
+      json(),
+    ],
+  };
+}
+
+export default [getConfig({ browser: true })];
